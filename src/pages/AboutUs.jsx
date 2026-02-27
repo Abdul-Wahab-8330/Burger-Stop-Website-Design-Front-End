014b5a3b8b7d8727190c4bc7eeee7fb46c828718
Abdul Wahab <abdulwahabee5@gmail.com>
2026-02-28 02:32:56 +0500

    about us heading updated

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -16,9 +16,9 @@ function AboutUs() {
             <OrderOnlineBar />
 
             <div className="w-full bg-primary px-6 md:px-18 md:py-8 py-6">
-                <h2 className=" text-4xl md:text-7xl uppercase">
+                <h2 className=" text-4xl md:text-8xl uppercase">
                     <span className="text-secondary font-medium italic font-extrabold tracking-tighter" style={{ letterSpacing: "-0.005em", display: "inline-block" }}>ABOUT</span>
-                    <span className="text-secondary md:ml-5 ml-3 font-spatial-black tracking-tighter" style={{ WebkitTextStroke: "0.5px #1E1E1C", transform: "scaleX(1.1)", display: "inline-block" }}>US</span>
+                    <span className="text-secondary md:ml-8 ml-3 font-spatial-black tracking-tighter" style={{ WebkitTextStroke: "0.5px #1E1E1C", transform: "scaleX(1.1)", display: "inline-block" }}>US</span>
                 </h2>
             </div>
 
